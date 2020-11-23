92263b615c1c0d8cf18410a1096575c6f260b0cf
kiayria <karina.maikenova@gmail.com>
2020-11-23 14:40:19 +0300

    Modiefied tests for HW4.1

--- homework04/task01/tests/test_read_file.py
+++ homework04/task01/tests/test_read_file.py
@@ -1,63 +1,61 @@
 import os
+import tempfile
 
 import pytest
 from read_file.read_file import read_magic_number
 
 
-@pytest.fixture
-def setup():
-    temp_file = open("test_data.txt", "w")
-    yield
-    temp_file.close()
-    os.remove("test_data.txt")
+def prepare_file(content):
+    def decorate(f):
+        def wrapper():
+            with tempfile.NamedTemporaryFile(mode="w") as tmpfile:
+                tmpfile.write(content)
+                tmpfile.flush()
+                return f(file_path=tmpfile.name)
+
+        return wrapper
+
+    return decorate
 
 
-@pytest.mark.usefixtures("setup")
-@pytest.mark.parametrize(
-    "numbers",
-    [
-        1,
-        2.5,
-        2.99,
-    ],
-)
-def test_read_magic_number_positive(numbers):
-    with open("test_data.txt", "w") as f:
-        f.write(f"{numbers}\n")
-    path = os.path.join(os.path.dirname(__file__), "test_data.txt")
-    assert read_magic_number(path) is True
+@prepare_file(content="1\n2\n3")
+def test_read_magic_number_positive_lower_boundary(file_path):
+    """Testing read_magic_number() with a number that belongs to the interval"""
+    assert os.path.exists(file_path)
+    assert read_magic_number(file_path) is True
 
 
-@pytest.mark.usefixtures("setup")
-@pytest.mark.parametrize(
-    "numbers",
-    [
-        0,
-        0.99,
-        3,
-    ],
-)
-def test_read_magic_number_negative(numbers):
-    with open("test_data.txt", "w") as f:
-        f.write(f"{numbers}\n")
-    path = os.path.join(os.path.dirname(__file__), "test_data.txt")
-    assert read_magic_number(path) is False
+@prepare_file(content="2\n1\n3")
+def test_read_magic_number_positive_upper_boundary(file_path):
+    """Testing read_magic_number() with a number that belongs to the interval"""
+    assert os.path.exists(file_path)
+    assert read_magic_number(file_path) is True
 
 
-@pytest.mark.usefixtures("setup")
-@pytest.mark.parametrize(
-    "text",
-    [
-        "not a number",
-        "8-xxx",
-    ],
-)
-def test_read_magic_number_exception(text):
-    with open("test_data.txt", "w") as f:
-        f.write(f"{text}\n")
-    path = os.path.join(os.path.dirname(__file__), "test_data.txt")
+@prepare_file(content="0\n1\n3")
+def test_read_magic_number_negative_lower_boundary(file_path):
+    """Testing read_magic_number() with a number that does not belong to the interval"""
+    assert os.path.exists(file_path)
+    assert read_magic_number(file_path) is False
+
+
+@prepare_file(content="3\n1\n3")
+def test_read_magic_number_negative_upper_boundary(file_path):
+    """Testing read_magic_number() with a number that does not belong to the interval"""
+    assert os.path.exists(file_path)
+    assert read_magic_number(file_path) is False
+
+
+@prepare_file(content="Text")
+def test_read_magic_number_exception_not_a_number(file_path):
     with pytest.raises(ValueError):
-        read_magic_number(path)
+        read_magic_number(file_path)
+
+
+@prepare_file(content="")
+def test_read_magic_number_exception_empty_file(file_path):
+    with pytest.raises(ValueError):
+        read_magic_number(file_path)
 
 
 def test_read_magic_number_path():
